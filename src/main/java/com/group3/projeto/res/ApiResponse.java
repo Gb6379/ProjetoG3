2aa692b2a6bb25cbc9337b9732c63a4f7a43e348
Gb6379 <gabriel.am.rosa@gmail.com>
2023-04-04 02:29:14 -0300

    api response class

---
+++ src/main/java/com/group3/projeto/res/ApiResponse.java
@@ -0,0 +1,10 @@
+package com.group3.projeto.res;
+
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+public class ApiResponse {
+
+    private final boolean success;
+    private final String message;
+}
