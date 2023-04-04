a3ae6087e0ef5f25efeefb69fcaa0a12252d9ad3
Gb6379 <gabriel.am.rosa@gmail.com>
2023-04-04 02:27:41 -0300

    paypal integration

--- src/main/java/com/group3/projeto/Controllers/PaymentController.java
+++ src/main/java/com/group3/projeto/Controllers/PaymentController.java
@@ -1,9 +1,59 @@
 package com.group3.projeto.Controllers;
 
+import com.group3.projeto.dto.CheckoutDto;
+import com.group3.projeto.services.PaymentService;
+import com.paypal.api.payments.Links;
+import com.paypal.base.rest.PayPalRESTException;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import com.paypal.api.payments.Payment;
 
 @RestController
 @RequiredArgsConstructor
 public class PaymentController {
+
+    @Autowired
+    PaymentService paymentService;
+
+    public static final String SUCCESS_URL = "pay/success";
+    public static final String CANCEL_URL = "pay/cancel";
+
+    @PostMapping("/pay")
+    public String payment(@ModelAttribute("checkout") CheckoutDto checkout) {
+        try {
+            Payment payment = paymentService.createPayment(checkout.getTotal(), checkout.getCurrency(), checkout.getMethod(),
+                    checkout.getIntent(), "http://localhost:9090/" + CANCEL_URL,
+                    "http://localhost:9090/" + SUCCESS_URL);
+            for(Links link:payment.getLinks()) {
+                if(link.getRel().equals("approval_url")) {
+                    return "redirect:"+link.getHref();
+                }
+            }
+
+        } catch (PayPalRESTException e) {
+
+            e.printStackTrace();
+        }
+        return "cancel";
+    }
+
+    @GetMapping(value = CANCEL_URL)
+    public String cancelPay() {
+        return "cancel";
+    }
+
+    @GetMapping(value = SUCCESS_URL)
+    public String successPay(@RequestParam("paymentId") String paymentId, @RequestParam("PayerID") String payerId) {
+        try {
+            Payment payment = paymentService.executePayment(paymentId, payerId);
+            System.out.println(payment.toJSON());
+            if (payment.getState().equals("approved")) {
+                return "success";
+            }
+        } catch (PayPalRESTException e) {
+            System.out.println(e.getMessage());
+        }
+        return "success";
+    }
 }
--- src/main/java/com/group3/projeto/services/PaymentService.java
+++ src/main/java/com/group3/projeto/services/PaymentService.java
@@ -1,11 +1,63 @@
 package com.group3.projeto.services;
 
+
+import com.paypal.api.payments.*;
+import com.paypal.base.rest.APIContext;
+import com.paypal.base.rest.PayPalRESTException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 @Slf4j
 public class PaymentService {
+
+    @Autowired
+    APIContext apiContext;
+
+    public Payment createPayment(Double total,
+                                 String currency,
+                                 String method,
+                                 String intent,
+                                 String cancelUrl, String successUrl) throws PayPalRESTException, PayPalRESTException {
+        Amount amount = new Amount();
+        amount.setCurrency(currency);
+
+        Transaction transaction = new Transaction();
+        transaction.setAmount(amount);
+
+        List<Transaction> transactions = new ArrayList<>();
+        transactions.add(transaction);
+
+        Payer payer = new Payer();
+        payer.setPaymentMethod(method.toString());
+
+        Payment payment = new Payment();
+        payment.setIntent(intent.toString());
+        payment.setPayer(payer);
+        payment.setTransactions(transactions);
+        RedirectUrls redirectUrls = new RedirectUrls();
+        redirectUrls.setCancelUrl(cancelUrl);
+        redirectUrls.setReturnUrl(successUrl);
+        payment.setRedirectUrls(redirectUrls);
+
+
+        return payment.create(apiContext);
+
+    }
+
+
+    public Payment executePayment(String paymentId, String payerId) throws PayPalRESTException{
+        Payment payment = new Payment();
+        payment.setId(paymentId);
+        PaymentExecution paymentExecute = new PaymentExecution();
+        paymentExecute.setPayerId(payerId);
+        return payment.execute(apiContext, paymentExecute);
+    }
+
 }
